bf0bbad0b747508842bc109f8be6605449da5f5f
nplaydev <matthew@n-play.com>
2014-09-29 15:37:46 -0400

    Secondary Commit

---
+++ prototype/scripts/app/search-prototype.js
@@ -0,0 +1,18 @@
+﻿var homeSearch = homeSearch || {};
+(function (ui) {	
+
+    var app = homeSearch;
+
+	app.init = function(){
+	    app.get();
+	};
+	app.get = function () {
+
+	};
+
+
+})(homeSearch);
+
+$(document).ready(function(){
+	homeSearch.init();
+});
